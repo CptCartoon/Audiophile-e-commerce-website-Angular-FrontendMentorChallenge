69001403ec71d40c1915e7a6b1f95e7d7dbf90a9
pstawiar <pstawiar@gmail.com>
2024-06-26 21:51:08 +0200

    header desktop ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HeaderComponent],
 })
 export class AppComponent {
   title = 'audiophile-ecommerce-website';
