4f4a3b8789b33783287afc5312d129ffcfa2b626
CptCartoon <noobski87@gmail.com>
2024-08-12 20:25:26 +0200

    item counter works

--- src/app/components/addtocart-counter/addtocart-counter.component.ts
+++ src/app/components/addtocart-counter/addtocart-counter.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { Component, ElementRef, Input, input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-addtocart-counter',
@@ -9,5 +9,20 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './addtocart-counter.component.scss',
 })
 export class AddtocartCounterComponent {
+  @ViewChild('countValue') countValue!: ElementRef;
   @Input() cart: boolean = false;
+
+  value = 0;
+
+  increment() {
+    this.countValue.nativeElement.value++;
+  }
+
+  decrement() {
+    if (this.countValue.nativeElement.value <= 0) {
+      this.countValue.nativeElement.value = 0;
+    } else {
+      this.countValue.nativeElement.value--;
+    }
+  }
 }
